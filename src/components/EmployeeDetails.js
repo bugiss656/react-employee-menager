2371fb7027349964e7d943a30efa039bee8015cc
mbugiel656 <mbugiel656@gmail.com>
2021-02-26 23:54:10 +0100

    Add features to EmployeeDetails component

--- src/components/EmployeeDetails.js
+++ src/components/EmployeeDetails.js
@@ -6,8 +6,31 @@ const EmployeeDetails = ({ employees }) => {
     const current_employee = employees.filter(employee => employee.id === parseInt(id));
 
     return (
-        <section>
-            <h3>{`${current_employee[0].first_name} ${current_employee[0].last_name}`}</h3>
+        <section className="page-section">
+            <h3>Profile details</h3>
+            <hr/>
+            <div className="d-flex flex-row employee-details">
+                <div className="d-flex flex-column profile-img align-items-center">
+                    <i className="bi bi-person-circle"></i>
+                    <button className="btn btn-outline-secondary">Update profile info</button>
+                </div>
+                <div className="profile-general-info">
+                    <h5>General informations:</h5>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item"><b>First name: </b>{current_employee[0].first_name}</li>
+                        <li className="list-group-item"><b>Last name: </b>{current_employee[0].last_name}</li>
+                        <li className="list-group-item"><b>Position: </b>{current_employee[0].position}</li>
+                        <li className="list-group-item"><b>Address: </b>{current_employee[0].address}</li>
+                        <li className="list-group-item"><b>Phone number: </b>{current_employee[0].phone_nr}</li>
+                    </ul>
+                </div>
+                <div className="profile-work-info">
+                    <h5>Work informations:</h5>
+                    <ul className="list-group list-group-flush">
+
+                    </ul>
+                </div>
+            </div>
         </section>
     );
 }
