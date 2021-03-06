0b4c77ebf57b229f6fd82f50f8de25db0be89aae
mbugiel656 <mbugiel656@gmail.com>
2021-03-06 16:45:34 +0100

    Add confirmation modal in case of deleting employee profile

--- src/components/EmployeeDetails.js
+++ src/components/EmployeeDetails.js
@@ -28,7 +28,24 @@ const EmployeeDetails = () => {
                         <Link to={`/updateemployee/${employee.id}`}>
                             <button className="btn btn-outline-secondary">Update profile info</button>
                         </Link>      
-                        <button onClick={deleteEmployee} className="btn btn-outline-danger">Delete profile</button> 
+                        <button className="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Delete profile</button>
+                        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div className="modal-dialog modal-dialog-centered">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5>Delete employee</h5>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <p>Are you sure you want to delete <b>{`${employee.first_name} ${employee.last_name}`}</b> employee?</p>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button onClick={deleteEmployee} type="button" className="btn btn-danger" data-bs-dismiss="modal">Delete</button>
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="profile-general-info">
                         <h5>General informations:</h5>
